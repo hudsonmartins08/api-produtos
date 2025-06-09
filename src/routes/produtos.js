b7384c1c8c10615813703f2746b594b38074611f
hudsonmartins08 <hudsonmartins108@gmail.com>
2025-06-09 12:09:13 -0300

    api add

--- src/routes/produtos.js
+++ src/routes/produtos.js
@@ -4,12 +4,21 @@ const produtoController = require('../controllers/produtos')
 const produtosMiddleware = require('../middlewares/produtos')
 const authMiddleware = require('../middlewares/auth')
 
-router.get('/produtos',authMiddleware.validateToken, produtoController.getProdutos)
-router.post('/produtos', produtosMiddleware.validateCreateProduto, produtoController.createProduto)
+router.get('/produtos',
+     authMiddleware.validateToken,
+      produtoController.getProdutos
+    )
+    
+router.post('/produtos',
+        authMiddleware.validateToken,
+        produtosMiddleware.validateCreateProduto,
+         produtoController.createProduto
+    )
 
 router.delete('/produtos/:id',
+    authMiddleware.validateToken,
     produtosMiddleware.validateDeleteProduto,
     produtoController.deleteProduto
-)
+    )
 
 module.exports = router;
